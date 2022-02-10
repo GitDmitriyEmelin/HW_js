f928b7b1236c9bf25903d20bc1ab46cc6223fe6c
GitDmitriyEmelin <82334342+GitDmitriyEmelin@users.noreply.github.com>
2022-02-10 20:04:16 +0300

    completed and added task 1 in lesson 5

--- lesson 5/1.js
+++ lesson 5/1.js
@@ -3,3 +3,55 @@
 // и белые ячейки. Строки должны нумероваться числами от 1 до 8, 
 // столбцы — латинскими буквами A, B, C, D, E, F, G, H.
 
+function generateBoard() {
+    let board = document.getElementById("board");
+    
+    // генерация строки с буквами
+    let charArray = ['A','B','C','D','E','F','G','H'];
+    let charEl = document.createElement("div");
+    let rowElement = document.createElement("div");
+    rowElement.className = 'row';
+    charEl.classList.add('cell');
+    rowElement.appendChild(charEl);
+    
+    for (let char of charArray) {
+        let charEl = document.createElement("div");
+        charEl.classList.add('cell');
+        charEl.innerHTML = char;
+        rowElement.appendChild(charEl);
+    }
+    board.appendChild(rowElement);
+
+    // генерация строк шахматной доски (поля)
+    for (let i = 8; i > 0; i--) {
+        let rowElement = document.createElement("div");
+        rowElement.className = 'row';
+        let numberElement = document.createElement("div");
+        numberElement.classList.add('cell');
+        numberElement.innerHTML = i;
+        rowElement.appendChild(numberElement);
+        for (let j = 1; j <= 8; j++) {
+            let cellElement = document.createElement("div");
+            cellElement.classList.add('cell');
+            if (isEven(i)) {
+                if (isEven(j)) {
+                    cellElement.classList.add('black');
+                } else {
+                    cellElement.classList.add('white');
+                }       
+            } else {
+                if (isEven(j)) {
+                    cellElement.classList.add('white');
+                } else {
+                    cellElement.classList.add('black');
+                }       
+            }
+            rowElement.appendChild(cellElement);
+        }
+        board.appendChild(rowElement);
+    }
+}
+function isEven(number) {
+    return number % 2 === 0;
+}
+generateBoard()
